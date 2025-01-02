799a5259ef068aadc350a147331f04e253b9fd6a
chaeli <s.kcl5243@gmail.com>
2025-01-03 02:46:28 +0900

    CHORE: setup

---
+++ src/main/java/com/likelion/JoinUP/JoinUpApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.JoinUP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/JoinUP/JoinUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.JoinUP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
